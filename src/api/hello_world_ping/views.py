d977e172761a4f659faa91333a206ec7288817a3
Hieupc <71920934+Hieucaohd@users.noreply.github.com>
2022-12-17 14:28:09 +0700

    update

--- src/api/hello_world_ping/views.py
+++ src/api/hello_world_ping/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, make_response, jsonify
+from flask import Blueprint, make_response, jsonify, Response, request
 
 from src.api import HttpMethod
 from src.common.utils.docs_register import register_view
@@ -9,19 +9,38 @@ blueprint = Blueprint('hello_world_ping', __name__)
 
 @blueprint.route(Endpoint.HELLO_WORLD, methods=[HttpMethod.GET])
 def hello_world():
-    response = make_response("Hello world")
-    response.set_cookie("my_fisrt_cookie", "valuehhhh", samesite=None)
+    return "Hello World"
+
+
+@blueprint.route('/set-cookie', methods=['GET'])
+def set_cookie():
+    response = Response('You have set a cookie')
+    response.set_cookie('id_token', 'somereallycoolvalue')
+
     return response
 
 
-@blueprint.route("/test-cookie", methods=[HttpMethod.GET])
-def test_cookie():
-    response = jsonify({
-        "hello": "world"
-    })
-    response.set_cookie("test_cookie", value="hello")
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
+@blueprint.route('/get-cookie', methods=['GET'])
+def get_cookie():
+    cookie = request.cookies.get('id_token')
+
+    if cookie is None:
+        res = 'Cookie not set'
+    else:
+        res = f'Cookie: {cookie}'
+
+    return res
+
+
+@blueprint.route('/remove-cookie', methods=['GET'])
+def remove_cookie():
+    if request.cookies.get('id_token'):
+        res = Response('id_token has been unset')
+        res.delete_cookie('id_token')
+    else:
+        res = 'id_token was not a cookie'
+
+    return res
 
 
 def register_docs(docs):
